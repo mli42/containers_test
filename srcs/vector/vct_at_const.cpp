34679fa857c9405b6888266a88efb5c27b270528
mli42 <mli@student.42.fr>
2021-02-01 14:22:20 +0100

    vct_at_const

---
+++ srcs/vector/vct_at_const.cpp
@@ -0,0 +1,23 @@
+#include "common.hpp"
+
+#define TESTED_TYPE int
+
+int		main(void)
+{
+	TESTED_NAMESPACE::vector<TESTED_TYPE> vct(7);
+
+	for (unsigned long int i = 0; i < vct.size(); ++i)
+	{
+		vct.at(i) = (vct.size() - i) * 3;
+		std::cout << "vct[]: " << vct[i] << std::endl;
+	}
+	printSize(vct);
+
+	TESTED_NAMESPACE::vector<TESTED_TYPE> const vct_c(vct);
+
+	std::cout << "front(): " << vct.front() << " " << vct_c.front() << std::endl;
+	std::cout << "back(): " << vct.back() << " " <<  vct_c.back() << std::endl;
+
+	vct_c.at(10);
+	return (0);
+}
