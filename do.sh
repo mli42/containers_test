220d00bda83396144ae7e511e7e41396c7b19059
mli42 <mli@student.42.fr>
2021-02-01 12:19:51 +0100

    Color on emojis on linux

--- do.sh
+++ do.sh
@@ -38,7 +38,7 @@ compile () {
 
 printRes () {
 	# 1=file 2=compile 3=bin 4=output 5=std_compile
-	b[0]="✅"; b[1]="❌";
+	b[0]="\e[1;92m✅\e[m"; b[1]="\e[1;91m❌\e[m";
 	s_bool[0]="Y"; s_bool[1]="N";
 	printf "%-35s: COMPILE: ${b[$2]} | RET: ${b[$3]} | OUT: ${b[$4]} | STD: [${s_bool[$5]}]\n" $1
 }
