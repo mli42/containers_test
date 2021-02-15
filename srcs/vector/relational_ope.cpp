0052b910c96b044eac7627702d99d24a5adf9b9f
Marc <marc.li5101@gmail.com>
2021-02-15 16:29:13 +0100

    vector: relational_operators

---
+++ srcs/vector/relational_ope.cpp
@@ -0,0 +1,22 @@
+#include "common.hpp"
+
+#define TESTED_TYPE int
+
+template <class T, class Alloc>
+void	cmp(const TESTED_NAMESPACE::vector<T, Alloc> &lhs, const TESTED_NAMESPACE::vector<T, Alloc> &rhs)
+{
+	static int i = 0;
+
+	std::cout << "############### [" << i << "] ###############"  << std::endl;
+	std::cout << "eq: " << (lhs == rhs) << " | ne: " << (lhs != rhs) << std::endl;
+	std::cout << "lt: " << (lhs <  rhs) << " | le: " << (lhs <= rhs) << std::endl;
+	std::cout << "gt: " << (lhs >  rhs) << " | ge: " << (lhs >= rhs) << std::endl;
+}
+
+int		main(void)
+{
+	TESTED_NAMESPACE::vector<TESTED_TYPE> vct(4);
+
+	cmp(vct, vct);
+	return (0);
+}
