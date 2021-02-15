4bf164a919e1bedd252a86b02a78f75e12e204c0
mli <mli@student.42.fr>
2021-02-16 00:26:14 +0100

    vector non members methods OK

--- srcs/vector/relational_ope.cpp
+++ srcs/vector/relational_ope.cpp
@@ -7,7 +7,7 @@ void	cmp(const TESTED_NAMESPACE::vector<T, Alloc> &lhs, const TESTED_NAMESPACE::
 {
 	static int i = 0;
 
-	std::cout << "############### [" << i << "] ###############"  << std::endl;
+	std::cout << "############### [" << i++ << "] ###############"  << std::endl;
 	std::cout << "eq: " << (lhs == rhs) << " | ne: " << (lhs != rhs) << std::endl;
 	std::cout << "lt: " << (lhs <  rhs) << " | le: " << (lhs <= rhs) << std::endl;
 	std::cout << "gt: " << (lhs >  rhs) << " | ge: " << (lhs >= rhs) << std::endl;
@@ -16,7 +16,25 @@ void	cmp(const TESTED_NAMESPACE::vector<T, Alloc> &lhs, const TESTED_NAMESPACE::
 int		main(void)
 {
 	TESTED_NAMESPACE::vector<TESTED_TYPE> vct(4);
+	TESTED_NAMESPACE::vector<TESTED_TYPE> vct2(4);
 
-	cmp(vct, vct);
+	cmp(vct, vct);  // 0
+	cmp(vct, vct2); // 1
+
+	vct2.resize(10);
+
+	cmp(vct, vct2); // 2
+	cmp(vct2, vct); // 3
+
+	vct[2] = 42;
+
+	cmp(vct, vct2); // 4
+	cmp(vct2, vct); // 5
+
+	swap(vct, vct2);
+
+	cmp(vct, vct2); // 6
+	cmp(vct2, vct); // 7
+
 	return (0);
 }
