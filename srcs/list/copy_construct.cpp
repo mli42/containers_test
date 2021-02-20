3340dcae596dfb0ed48962ffa831d8e7a610bf61
mli <mli@student.42.fr>
2021-02-20 20:38:34 +0100

    lst/vct copy construct more thorough

--- srcs/list/copy_construct.cpp
+++ srcs/list/copy_construct.cpp
@@ -4,21 +4,35 @@
 
 int		main(void)
 {
-	const int size = 5;
-	TESTED_NAMESPACE::list<TESTED_TYPE> lst(size);
-	TESTED_NAMESPACE::list<TESTED_TYPE>::iterator it = lst.begin();
-	TESTED_NAMESPACE::list<TESTED_TYPE>::iterator ite = lst.end();
-
+	TESTED_NAMESPACE::list<TESTED_TYPE> lst(5);
+	TESTED_NAMESPACE::list<TESTED_TYPE>::iterator it = lst.begin(), ite = lst.end();
 	for (int i = 0; it != ite; ++it)
 		*it = ++i;
 
 	it = lst.begin();
-	TESTED_NAMESPACE::list<TESTED_TYPE> lst_cpy(it, ite);
-
+	TESTED_NAMESPACE::list<TESTED_TYPE> lst_range(it, --(--ite));
 	for (int i = 0; it != ite; ++it)
 		*it = ++i * 5;
 
+	it = lst.begin();
+	TESTED_NAMESPACE::list<TESTED_TYPE> lst_copy(lst);
+	for (int i = 0; it != ite; ++it)
+		*it = ++i * 7;
+	lst_copy.push_back(42);
+	lst_copy.push_back(21);
+
+	std::cout << "\t-- PART ONE --" << std::endl;
 	printSize(lst);
-	printSize(lst_cpy);
+	printSize(lst_range);
+	printSize(lst_copy);
+
+	lst = lst_copy;
+	lst_copy = lst_range;
+	lst_range.clear();
+
+	std::cout << "\t-- PART TWO --" << std::endl;
+	printSize(lst);
+	printSize(lst_range);
+	printSize(lst_copy);
 	return (0);
 }
---
+++ srcs/vector/copy_construct.cpp
@@ -0,0 +1,41 @@
+#include "common.hpp"
+
+#define TESTED_TYPE int
+
+int		main(void)
+{
+	TESTED_NAMESPACE::vector<TESTED_TYPE> vct(5);
+	TESTED_NAMESPACE::vector<TESTED_TYPE>::iterator it = vct.begin(), ite = vct.end();
+
+	std::cout << "len: " << (ite - it) << std::endl;
+	for (; it != ite; ++it)
+		*it = (ite - it);
+
+	it = vct.begin();
+	TESTED_NAMESPACE::vector<TESTED_TYPE> vct_range(it, --(--ite));
+	for (int i = 0; it != ite; ++it)
+		*it = ++i * 5;
+
+	it = vct.begin();
+	TESTED_NAMESPACE::vector<TESTED_TYPE> vct_copy(vct);
+	for (int i = 0; it != ite; ++it)
+		*it = ++i * 7;
+	vct_copy.push_back(42);
+	vct_copy.push_back(21);
+
+	std::cout << "\t-- PART ONE --" << std::endl;
+	printSize(vct);
+	printSize(vct_range);
+	printSize(vct_copy);
+
+	vct = vct_copy;
+	vct_copy = vct_range;
+	vct_range.clear();
+
+	std::cout << "\t-- PART TWO --" << std::endl;
+	printSize(vct);
+	printSize(vct_range);
+	printSize(vct_copy);
+	return (0);
+}
+
