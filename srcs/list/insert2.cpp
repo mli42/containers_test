18e3521219d98e33b9079844304b3c12a605fed8
mli <mli@student.42.fr>
2021-02-21 20:06:23 +0100

    list/insert2 more readable

--- srcs/list/insert2.cpp
+++ srcs/list/insert2.cpp
@@ -6,18 +6,23 @@ int		main(void)
 {
 	TESTED_NAMESPACE::list<TESTED_TYPE> lst(5);
 	TESTED_NAMESPACE::list<TESTED_TYPE> lst2;
-	const int cut = 3;
+	TESTED_NAMESPACE::list<TESTED_TYPE> lst3(5);
 	TESTED_NAMESPACE::list<TESTED_TYPE>::iterator it = lst.begin();
+	const int cut = 3;
 
 	for (unsigned long int i = 0; i < lst.size(); ++i)
 		*it++ = (lst.size() - i) * 7;
+	it = lst3.begin();
+	for (unsigned long int i = 0; i < lst3.size(); ++i)
+		*it++ = i;
 	printSize(lst);
+	printSize(lst3);
 
 	lst2.insert(lst2.begin(), lst.begin(), inc(lst.begin(), cut));
 	printSize(lst2);
 	lst2.insert(lst2.begin(), inc(lst.begin(), cut), lst.end());
 	printSize(lst2);
-	lst2.insert(lst2.end(), lst.begin(), inc(lst.begin(), cut));
+	lst2.insert(lst2.end(), lst3.begin(), inc(lst3.begin(), cut));
 	printSize(lst2);
 
 	std::cout << "insert return:" << std::endl;
