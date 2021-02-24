801c62c4c1c17d5be68dbd8626516f2beebea575
Marc <marc.li5101@gmail.com>
2021-02-24 10:43:06 +0100

    Verbose class activate for splice

--- srcs/list/splice.cpp
+++ srcs/list/splice.cpp
@@ -39,12 +39,23 @@ int		main(void)
 	TESTED_NAMESPACE::list<TESTED_TYPE> lst, lst2;
 	TESTED_NAMESPACE::list<TESTED_TYPE>::iterator it;
 
+	// Fill lists
 	for (int i = 1; i <= 4; ++i)
 		lst.push_back(i * 1);
 	for (int i = 1; i <= 3; ++i)
 		lst2.push_back(i * 10);
 	printSize(lst);
 	printSize(lst2);
+
+	// Enable verbose for all
+	for (it = lst.begin(); it != lst.end(); ++it)
+		it->switchVerbose();
+	for (it = lst2.begin(); it != lst2.end(); ++it)
+		it->switchVerbose();
+
+	*lst.begin() = TESTED_TYPE(42); // Outputs something
+
+	// Start testing splice
 	it = ++lst.begin();
 
 	ft_splice(lst, it, lst2);
