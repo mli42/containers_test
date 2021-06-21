d655c28b869f6ca192cb66d475c580e259626c50
mli42 <mli@student.42.fr>
2021-06-21 16:57:25 +0200

    Stop printing Green + N: when std has a problem with included file

--- fct.sh
+++ fct.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+EOC="\e[0m"
+BOLD="\e[1m"
+RED="\e[91m"
+GREEN="\e[92m"
+YELLOW="\e[93m"
+DBLUE="\e[94m"
+PURPLE="\e[95m"
+CYAN="\e[96m"
+DGREY="\e[1;90m"
+
 tested_path="../"
 incl_path="$tested_path"
 srcs="srcs"
@@ -28,14 +38,14 @@ printf "\e[0;1;94m\
 }
 
 compile () {
-	# 1=file 2=define used {ft/std} 3=output_file
-	$CC $CFLAGS -o "$3" -I./$incl_path -DTESTED_NAMESPACE=$2 $1 2>/dev/null 1>&2
+	# 1=file 2=define used {ft/std} 3=output_file 4=compile_log
+	$CC $CFLAGS -o "$3" -I./$incl_path -DTESTED_NAMESPACE=$2 $1 &>$4
 	return $?
 }
 
 printRes () {
 	# 1=file 2=compile 3=bin 4=output 5=std_compile
-	b[0]="\e[1;92m✅\e[m"; b[1]="\e[1;91m❌\e[m";
+	b[0]="${BOLD}${GREEN}✅${EOC}"; b[1]="${BOLD}${RED}❌${EOC}";
 	s_bool[0]="Y"; s_bool[1]="N";
 	printf "%-35s: COMPILE: ${b[$2]} | RET: ${b[$3]} | OUT: ${b[$4]} | STD: [${s_bool[$5]}]\n" $1
 }
@@ -54,11 +64,20 @@ cmp_one () {
 	testname=$(echo $file | cut -d "." -f 1)
 	ft_bin="ft.$container.out"; ft_log="$logdir/ft.$testname.$container.log"
 	std_bin="std.$container.out"; std_log="$logdir/std.$testname.$container.log"
+	std_compile_log="std.$testname.$container.compile.log"
 
-	compile "$1" "ft"  "$ft_bin";  ft_ret=$?
-	compile "$1" "std" "$std_bin"; std_ret=$?
+	compile "$1" "ft"  "$ft_bin" /dev/null;  ft_ret=$?
+	compile "$1" "std" "$std_bin" $std_compile_log; std_ret=$?
 	same_compilation=$(isEq $ft_ret $std_ret)
 	std_compile=$std_ret
+
+	if [ $std_compile -ne 0 ] && \
+		cat $std_compile_log | grep "$container/common.hpp:1" &>/dev/null; then
+		rm -f $std_compile_log
+		printf "${BOLD}${PURPLE}[$container/$testname] Cannot compile, please use \`./one\` to debug${EOC}\n"
+		return;
+	fi
+	rm -f $std_compile_log
 
 	> $ft_log; > $std_log;
 	if [ $ft_ret -eq 0 ]; then
