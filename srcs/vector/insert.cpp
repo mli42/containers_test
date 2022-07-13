8eba0bf59d685ce91c6728a6891d6be48f898622
ammonschiffer <ammon.pro@gmail.com>
2022-07-13 22:37:10 +0200

    insert fix

--- srcs/vector/insert.cpp
+++ srcs/vector/insert.cpp
@@ -18,6 +18,7 @@ int		main(void)
 {
 	TESTED_NAMESPACE::vector<TESTED_TYPE> vct(10);
 	TESTED_NAMESPACE::vector<TESTED_TYPE> vct2;
+	TESTED_NAMESPACE::vector<TESTED_TYPE> vct3;
 
 	for (unsigned long int i = 0; i < vct.size(); ++i)
 		vct[i] = (vct.size() - i) * 3;
@@ -41,5 +42,11 @@ int		main(void)
 	printSize(vct2);
 
 	printSize(vct);
+
+	for (int i = 0; i < 5; ++i)
+		vct3.push_back(i);	
+	vct3.insert(vct3.begin() + 1, 2, 111);
+	printSize(vct3);
+
 	return (0);
 }
